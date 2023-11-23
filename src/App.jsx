3fe62d97378415e5e062194cff459378f4a03339
IamClodate <clodatemnisi@gmail.com>
2023-11-23 09:32:43 +0200

    imported Routes from "react-router-dom"  to import the neccessary pages from the navbars

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css';
 import Navbar from "./components/Navbar";
 import { Routes } from "react-router-dom";
+import Home from './routes/Home';
 
 export default function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route element={<Home/>}/>
+      </Routes>
           <Navbar/>
+          <Home/>
     </div>
   );
 }
---
+++ src/routes/tours.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const tours = () => {
+  return (
+    <div>
+    <h1>tours</h1>
+    </div>
+  )
+}
+
+export default tours
