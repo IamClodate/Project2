255e3229bd2e987c1c02d752cfe3a89655d22216
IamClodate <clodatemnisi@gmail.com>
2023-11-23 09:52:29 +0200

    added the BrowserRoter to be able to import pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import { Routes } from "react-router-dom";
 import Home from './routes/Home';
+import About from './routes/About';
+import Service from './routes/Service';
+import Tours from './routes/Tours';
+import Blog from './routes/Blog';
+import Contact from './routes/Contact';
+
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-        <Route element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/service" element={<Service/>}/>
+        <Route path="/tours" element={<Tours/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
           <Navbar/>
           <Home/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { StrictMode } from "react";
+import { CreateRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
+
+
