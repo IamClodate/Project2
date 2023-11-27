5107efef4d8857962c8db5b27ae2aeb9a6b65e0d
IamClodate <clodatemnisi@gmail.com>
2023-11-27 13:27:37 +0200

    updated the About.jsx page by importing images and the hero section and styled it

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,5 +1,6 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import AboutImg from  "../assets/sea.jpeg";
 
 
 function About () {
@@ -7,7 +8,7 @@ function About () {
       <div>
         <Hero 
           cName="hero"
-          heroImg="https://images.unsplash.com/photo-1504150558240-0b4fd8946624?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D"
+          heroImg={ AboutImg}   
           title="Explore your heart desires"
           text="Travel The world"
           buttonText="Explore More"
