eaaae69c4f1bf9945713f1530ec954ae86bbbb90
IamClodate <clodatemnisi@gmail.com>
2023-11-28 11:27:35 +0200

    added the Navbars to every page from the navbars

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,6 @@ function Home () {
   return (
     <div>
         <Navbar />
-        <h1>Home</h1>
         <Hero 
           cName="hero"
           heroImg="https://images.unsplash.com/photo-1504150558240-0b4fd8946624?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D"
