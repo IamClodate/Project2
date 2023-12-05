252e5fd9324ceb87664f95de50cf70028a81f102
IamClodate <clodatemnisi@gmail.com>
2023-12-05 14:01:23 +0200

    added props into the Hero function to make everything to be dynamic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import "./Hero.css";
 // import herpImg from '../assets/image2.jpeg'; 
 
-function Hero() {
+function Hero(props) {
     return (
         
       // <div className={props.cName}>
@@ -16,13 +16,16 @@ function Hero() {
       //     </div>
       // </div>
       <>
-      <div className="hero">
+      <div className={props.CName}>
         <h1>Welcome</h1>
         <img alt="HerpImg" src="https://images.unsplash.com/photo-1464207687429-7505649dae38?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D"/>
          <div className="hero-text" >
           <h1>Welcome to Tranquil Tours</h1>
           <h2>Explore your heart desires</h2>
           <p>Thank you for visiting us</p>
+          <a href="/">
+            My Travel Tours
+          </a>
          </div>
         <h1>Test</h1>
       </div>
