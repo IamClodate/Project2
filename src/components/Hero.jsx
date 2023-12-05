3ab502ba0224d8d90e531e5c226d4a1c63dcfe57
IamClodate <clodatemnisi@gmail.com>
2023-12-05 14:21:49 +0200

    changed the image of the home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,12 +16,14 @@ function Hero(props) {
       //     </div>
       // </div>
       <>
-      <div className={props.CName}>
+      <div className={props.cName}>
         <img alt="HerpImg" src={props.heroImg}/>
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
+
+        
         <img alt="HerpImg" src="https://images.unsplash.com/photo-1464207687429-7505649dae38?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D"/>
          <div className="hero-text" >
+         <h1>{props.title}</h1>
+        <p>{props.text}</p>
           {/* <h1>Welcome to Tranquil Tours</h1>
           <h2>Explore your heart desires</h2>
           <p>Thank you for visiting us</p> */}
