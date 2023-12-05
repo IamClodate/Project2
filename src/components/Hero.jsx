afbc8648ed89cb72bee383d88d7eba952ca94e5d
IamClodate <clodatemnisi@gmail.com>
2023-12-05 13:46:50 +0200

    added the first welcome message and the second heading and paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,13 @@ function Hero() {
       // </div>
       <>
       <div className="hero">
+        <h1>Welcome</h1>
         <img alt="HerpImg" src="https://images.unsplash.com/photo-1464207687429-7505649dae38?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D"/>
+         <div className="hero-text" >
+          <h1>Welcome to Tranquil Tours</h1>
+          <h2>Explore your heart desires</h2>
+          <p>Thank you for visiting us</p>
+         </div>
         <h1>Test</h1>
       </div>
       </>
