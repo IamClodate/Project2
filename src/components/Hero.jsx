c155c3d20ab8d15af00263736dd03f50c37bc9d2
IamClodate <clodatemnisi@gmail.com>
2023-12-05 14:09:19 +0200

    added more props to make evrything to be dynamicon all pages from the navbars

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,8 @@ function Hero(props) {
       // </div>
       <>
       <div className={props.CName}>
-        <h1>Welcome</h1>
+        <img alt="HerpImg" src={props.heroImg}/>
+        <h1>{props.title}</h1>
         <img alt="HerpImg" src="https://images.unsplash.com/photo-1464207687429-7505649dae38?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D"/>
          <div className="hero-text" >
           <h1>Welcome to Tranquil Tours</h1>
