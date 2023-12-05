0335e5eddbb96036a457546e1cd0f6569e49027e
IamClodate <clodatemnisi@gmail.com>
2023-12-05 14:57:20 +0200

    changed the images of the pages from the navbars to have different images

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,6 +1,6 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
-import AboutImg from  "../assets/sea.jpeg";
+import AboutImg from  "../assets/images4.jpeg";
 
 
 function Contact () {
