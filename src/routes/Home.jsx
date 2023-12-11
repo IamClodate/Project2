52fbc01755f5d212fad513e77d418c9d3eb2e0e9
IamClodate <clodatemnisi@gmail.com>
2023-12-11 10:01:59 +0200

    removed the imports and hero section from the home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,13 @@
 import Desitination from "../components/Desitination";
-import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
+// import Hero from "../components/Hero";
+// import Navbar from "../components/Navbar";
 
 function Home () {
   return (
     <div>
       <div >
         {/* <Navbar /> */}
-        <Hero
+        {/* <Hero
           cName="hero"
           heroImg="https://images.unsplash.com/photo-1504150558240-0b4fd8946624?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D"
           title="Explore your heart desires Clody"
@@ -15,7 +15,7 @@ function Home () {
           buttonText="Explore More"
           url="/"
           btnClass="show"
-        />
+        /> */}
         <Desitination />
     </div>
     </div>
