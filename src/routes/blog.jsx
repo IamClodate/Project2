abb6e33ac2b6a1c77cbfd207debbf8928241c265
IamClodate <clodatemnisi@gmail.com>
2023-12-11 09:58:03 +0200

    removed the imports and hero section from blog page

--- src/routes/blog.jsx
+++ src/routes/blog.jsx
@@ -1,20 +1,20 @@
-import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
-import AboutImg from  "../assets/images3.jpeg";
+// import Hero from "../components/Hero";
+// import Navbar from "../components/Navbar";
+// import AboutImg from  "../assets/images3.jpeg";
 
 
 function Blog () {
     return (
       <div>
         {/* <Navbar /> */}
-        <Hero 
+        {/* <Hero 
         cName="hero-mid"
         heroImg={ AboutImg }   
         title="This is the Blog Page"
         buttonText="Explore More"
         
         btnClass="hide"
-        />
+        /> */}
       </div>
     )
   }
