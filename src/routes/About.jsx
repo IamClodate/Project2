94ca99989bc652ccad992c1d8764b7f26923c2bc
IamClodate <clodatemnisi@gmail.com>
2024-01-19 10:28:42 +0200

    added the import from the Destination

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,7 @@
 // import Hero from "../components/Hero";
 // import Navbar from "../components/Navbar";
 // import AboutImg from  "../assets/sea.jpeg";
-
+import Desitination from "../components/Desitination";
 
 function About() {
     return (
@@ -15,6 +15,18 @@ function About() {
           
           btnClass="hide"
         /> */}
+
+<div className="form-container">
+            {/* <h1>Contact Us</h1> */}
+
+            <Desitination />
+                <form>
+                    <textarea placeholder="Thank you for visiting us"></textarea>
+                    <button>Login</button><br></br>
+                    <button>Explore More</button>
+
+                </form>
+        </div>
       </div>
     );
   }
