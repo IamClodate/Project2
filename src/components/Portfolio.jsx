359c92c87edfef2f0dd77a0089bf8d6198d06b59
IamClodate <clodatemnisi@gmail.com>
2024-01-23 13:20:52 +0200

    created new classNames under each div to place all the right sections of the portfolio content

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,2 +1,22 @@
 import React from "react";
 import "./Portfolio.css";
+
+
+
+const Portfolio = () => {
+  return (
+    <div className="portfolio section container">
+        <div className="secContainer grid">
+            <div className="leftContent">
+                <div className="secHeading">
+                    <h3>Why Should you Choose Us</h3>
+                </div>
+            </div>
+        </div>
+        Portfolio
+
+    </div>
+  )
+}
+
+export default Portfolio
