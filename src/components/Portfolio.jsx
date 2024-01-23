e33ddbe91e4c6cf950ac592ba64e4029a502aadb
IamClodate <clodatemnisi@gmail.com>
2024-01-23 13:30:05 +0200

    added an image that was imported from the asset file under the src, to make the  blog page to be more appealing to the user

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./Portfolio.css";
 
 
+import picture from "../assets/sea.jpeg"
+
+
 
 const Portfolio = () => {
   return (
@@ -10,7 +13,17 @@ const Portfolio = () => {
             <div className="leftContent">
                 <div className="secHeading">
                     <h3>Why Should you Choose Us</h3>
-                    <p>We have extensive knowledge and experience in the travel industry</p>
+                    <p>We have extensive knowledge and experience in the travel industry....</p>
+                </div>
+
+                <div className="grid">
+                    <div className="singlePortfolio flex">
+                        <div className="iconDiv">
+                            <img></img>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
         </div>
