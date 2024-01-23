393223d7178e308fda923d71e04e8bc1bb784798
IamClodate <clodatemnisi@gmail.com>
2024-01-23 13:23:23 +0200

    added a new paragrapg message to tell the user more about our services in the travel guides

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,6 +10,7 @@ const Portfolio = () => {
             <div className="leftContent">
                 <div className="secHeading">
                     <h3>Why Should you Choose Us</h3>
+                    <p>We have extensive knowledge and experience in the travel industry</p>
                 </div>
             </div>
         </div>
