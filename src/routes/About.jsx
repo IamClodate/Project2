82557219855d73d5d03f148fcf7dd20939745abb
IamClodate <clodatemnisi@gmail.com>
2024-01-23 10:26:23 +0200

    removed the login button from the about page because it has already been placed on the navbars and the login page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -24,7 +24,7 @@ function About() {
                     <textarea placeholder="Thank you for visiting us"></textarea>
                     <textarea placeholder="This is the about us page"></textarea>
                     <textarea placeholder="About"></textarea>
-                    <button>Login</button><br></br>
+                    
                     <button>Explore More</button>
 
                 </form>
